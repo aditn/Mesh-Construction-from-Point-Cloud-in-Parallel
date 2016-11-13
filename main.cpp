520dd1136bd2cbf00b2d97c06e2c518078a113b5
jrtick <jrtick@andrew.cmu.edu>
2016-11-13 16:31:13 -0500

    just load/save stuff

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 /*I mean this could be c or c++ but I feel like c++ is nicer and lets us use cooler data structures*/
 
+/* needed for file processing */
 #include <iostream>
 #include <fstream>
 
+/* needed for vectors */
 #include <vector>
+
+/* needed for malloc and printf and scanf and what not */
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -13,6 +17,9 @@ typedef struct{
   float x,y,z;
 } Point;
 
+/* parseFile takes a filename as input, tests it as an obj, and grabs all of its vertices
+ * It returns of Point vector of these vertices
+ * An error will be thrown if file cannot be read */
 std::vector<Point> parseFile(const char* filename){
   std::vector<Point> points;
   std::ifstream fin;
@@ -38,7 +45,25 @@ std::vector<Point> parseFile(const char* filename){
   return points;
 }
 
-void printPoint(Point p){
+void saveMesh(Point* points,int numPoints, char* out_filename){
+  std::ofstream fout;
+  fout.open(out_filename);
+  if(!fout.good()){
+    printf("couldn't write to %s\n",out_filename);
+    exit(0);
+  }else{
+    for(int i=0;i<numPoints;i++){
+      float x,y,z;
+      x=points[i].x;
+      y=points[i].y;
+      z=points[i].z;
+      fout << "v " << x << " " << y << " " << z << "\n";
+    }
+  }
+  fout.close();
+}
+
+inline void printPoint(Point p){
   printf("(%.3f,%.3f,%.3f)\n",p.x,p.y,p.z);
 }
 
@@ -47,11 +72,21 @@ int main(){
   //until we decide how we're actually gonna grab kinect stuff
 
   //step 1: parse input file/stream
-  const char* filename = "test.obj";
-  std::vector<Point> vertices = parseFile(filename);
-  for(int i=0;i<(int)vertices.size();i++) printPoint(vertices[i]);
+  const char* in_filename = "inputs/test.obj";
+  std::vector<Point> vertices = parseFile(in_filename);
+  int numPoints = vertices.size();
+  Point* points = (Point*) malloc(sizeof(Point)*numPoints);
+  for(int i=0;i<numPoints;i++) points[i] = vertices[i];
+  for(int i=0;i<numPoints;i++) printPoint(points[i]);
   //step 2: create mesh in parallel
   //step 3: write mesh to output file
-
+  int slen = 0;
+  while(in_filename[slen++]!='\0');
+  char out_filename[slen+5];
+  for(int i=0;i<3;i++) out_filename[i] = "out"[i];
+  for(int i=2;i<slen-5;i++) out_filename[i+1]=in_filename[i];
+  for(int i=0;i<9;i++) out_filename[slen-4+i]="_out.obj"[i]; //9 includes null terminator
+  printf("saving to %s\n",out_filename);
+  saveMesh(points,numPoints,out_filename);
   return 0;
 }
