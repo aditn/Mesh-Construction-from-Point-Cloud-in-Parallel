d4c6507da43c3909c35144af4a717cc7682d53c0
Adit Namdev <aditn4@gmail.com>
2018-12-15 02:10:57 -0500

    first time setup on personal laptop. Removed junk source code in utils/ folder. Replaced utils/Eigen/ with git submodule to allow for easy update.

--- compile.bash
+++ compile.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
-g++ -Wall -std=c++11 -I util/ ourTimer.cpp main.cpp tangentPlane.cpp parseOBJ.cpp approximateMesh.cpp -o runnable -fopenmp
+g++ -Wall -std=c++11 -I util/eigen-git-mirror/ ourTimer.cpp main.cpp tangentPlane.cpp parseOBJ.cpp approximateMesh.cpp -o runnable -fopenmp
 #g++ -Wall -std=c++11 -I util/ testNormals.cpp -o PCA
